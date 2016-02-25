155e4dc787e627bf5fc042b837c9942d872e7750
Dmitry Ponyatov <dponyatov@gmail.com>
2016-02-25 16:30:50 +0400

    + IKS axioms
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -2,22 +2,41 @@
 #define YYERR "\n\n"<<yylineno<<":"<<msg<<"["<<yytext<<"]\n\n"
 void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
 int main() { return yyparse(); }
-//glob_init(); yyparse(); 
-//	cout<<module.eval()->dump()<<"\n\n"; return 0; }
-
 
 Sym::Sym(string T,string V) { tag=T; val=V; }
 Sym::Sym(string V):Sym("",V) {}
 void Sym::push(Sym*o) { nest.push_back(o); }
 
 string Sym::tagval() { return "<"+tag+":"+val+">"; }
-string Sym::dump() { string S = tagval();
-	for (auto it=nest.begin(),e=nest.end();it!=e;it++) S += (*it)->dump();
+string Sym::pad(int n) { string S; for (int i=0;i<n;i++) S+='\t'; return S; }
+string Sym::dump(int depth) { string S = "\n"+pad(depth)+tagval();
+	for (auto it=nest.begin(),e=nest.end();it!=e;it++)
+		S += (*it)->dump(depth+1);
 	return S; }
+void Sym::track() { cout << dump() << "\n------------------------"; }
 
-Combinator::Combinator(string V):Sym("",V) {}
-Var::Var(string V):Sym("v",V) {}
+Sym* Sym::eval() { track(); return this; }
+
 Const::Const(string V):Sym("c",V) {}
+Var::Var(string V):Sym("v",V) {}
+Combinator::Combinator(string V):Sym(V,"") {}
 
-App::App(Sym*A,Sym*B):Sym("@","") { push(A); push(B); }
+_I::_I(string V):Combinator(V) {}
+Sym* _I::eval() { track(); assert(nest.size()==1); return nest[0]; }
+
+_K::_K(string V):Combinator(V) {}
+Sym* _K::eval() { track(); assert(nest.size()==2); return nest[0]; }
+
+_S::_S(string V):Combinator(V) {}
+Sym* _S::eval() { track(); assert(nest.size()==3);
+	return new App(
+		new App(nest[0],nest[2]),
+		new App(nest[1],nest[2])
+			); }
+
+App::App(Sym*A,Sym*B):Sym("@","@") { push(A); push(B); }
+Sym* App::eval() { Sym::eval(); }
+
+LR::LR(Sym*A):Sym("(",")") { push(A); }
+Sym* LR::eval() { track(); assert(nest.size()==1); return nest[0]; }
 
--- hpp.hpp
+++ hpp.hpp
@@ -1,24 +1,38 @@
 #ifndef _H_IKS
 #define _H_IKS
-
+													// std.includes
 #include <iostream>
 #include <cstdlib>
+#include <cassert>
 #include <vector>
 using namespace std;
-
+													// Abstract Symbolic Type
 struct Sym {
-	string tag,val;
-	Sym(string,string); Sym(string);
-	vector<Sym*> nest; void push(Sym*);
-	string tagval(); string dump();
+	string tag,val;									// data class/type : value
+	Sym(string,string);								// T:V constructor
+	Sym(string);									// token constructor
+	vector<Sym*> nest; void push(Sym*);				// nest[]ed elements
+	string tagval(); 								// dump object in tree form
+	string dump(int depth=0); string pad(int);
+	virtual Sym* eval();							// evaluate/compute object
+	void track();									// track print
 };
 
-struct Const: Sym { Const(string); };
-struct Var: Sym { Var(string); };
-struct Combinator: Sym { Combinator(string); };
+struct Const: Sym { Const(string); };				// constant a..d
+struct Var: Sym { Var(string); };					// variable x..z
+struct Combinator: Sym { Combinator(string); };		// combinators:
+													// (base)
+struct _I: Combinator { _I(string); Sym*eval(); };	// Ix = x /identity/
+struct _K: Combinator { _K(string); Sym*eval(); };	// Kxy = x /constant/
+struct _S: Combinator { _S(string); Sym*eval(); };	// Sxyz = (xz)(yz) /fusion/
+													// (extra)
+struct _T: Combinator { _T(string); Sym*eval(); };	// Txyz = xzy /permutation/
+struct _Z: Combinator { _Z(string); Sym*eval(); };	// Zxyz = x(yz) /group/
 
-struct App: Sym { App(Sym*,Sym*); };
+struct App: Sym { App(Sym*,Sym*); Sym*eval(); };	// xy apply operation
+struct LR: Sym { LR(Sym*); Sym*eval(); };			// (x) brackets
 
+													// lexer/parser interface
 extern int yylex();
 extern int yylineno;
 extern char* yytext;
